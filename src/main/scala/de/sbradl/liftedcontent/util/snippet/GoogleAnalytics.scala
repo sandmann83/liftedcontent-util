7c327fa894d1b04135a94311d252b172f433ff9c
Stefan Bradl <stefan_bradl@web.de>
2012-02-09 22:56:29 +0100

    -

--- src/main/scala/de/sbradl/liftedcontent/util/snippet/GoogleAnalytics.scala
+++ src/main/scala/de/sbradl/liftedcontent/util/snippet/GoogleAnalytics.scala
@@ -5,17 +5,35 @@ import net.liftweb.http.S
 import net.liftweb.common.Full
 import net.liftweb.common.Empty
 import net.liftweb.util.Props
+import net.liftweb.common.Box
+import scala.xml.Text
+import scala.xml.NodeSeq
 
+/**
+ * Insert the google analytics tracking code into your page.
+ * 
+ * The snippet can be used with two attributes, "mode" and "id". The id specifies
+ * your google tracking id. Alternatively you can set it in your properties with the
+ * key "google.analytics.id". 
+ * The mode attribute can be used to turn off tracking.
+ * By default it is set to "Production", meaning that the tracking code is only
+ * rendered when in production mode.
+ */
 class GoogleAnalytics {
 
   def render = {
     val minimumRunMode = Props.RunModes.withName(S.attr("mode").openOr("Production"))
 
+    val id = trackingID
+
     "*" #> {
       Props.mode.id >= minimumRunMode.id match {
-        case true => S.attr("account") match {
-          case Full(account) => Full(scriptTag(account))
-          case _ => Empty
+        case true => id match {
+          case Full(trackingID) => Full(scriptTag(trackingID))
+          case _ => {
+            if (Props.mode.id <= Props.RunModes.Test.id) error
+            else Empty
+          }
         }
         case false => Empty
       }
@@ -23,9 +41,15 @@ class GoogleAnalytics {
     }
   }
 
-  def scriptTag(account: String) = <script type="text/javascript">
-                                     var _gaq = _gaq || [];
-	_gaq.push([ '_setAccount', '{account}' ]);
+  private def trackingID: Box[String] = S.attr("id") match {
+    case id @ Full(_) => id
+    case _ => Props.get("google.analytics.id")
+  }
+
+  private def scriptTag(trackingID: String) = 
+    <script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push([ '_setAccount', '{ trackingID }' ]);
 	_gaq.push([ '_trackPageview' ]);
 
 	(function() {{
@@ -37,6 +61,14 @@ class GoogleAnalytics {
 				+ '.google-analytics.com/ga.js';
 		var s = document.getElementsByTagName('script')[0];
 		s.parentNode.insertBefore(ga, s);
-	}})();
-                                   </script>
+	}})();</script>
+
+  private def error = errorDiv(<div>
+                                 No Tracking ID specified for the Google Analytics Snippet<br/>
+                                 Either specifiy the id attribute on snippet invocation 
+		  						 (e.g <b>GoogleAnalytics?id=my-tracking-id</b>) or set
+                                 <b>google.analytics.id</b>
+                                 in the properties
+                               </div>)
+
 }
