c5579cf9b4aeeafc846e4a7f8669ab5016acdd4a
Stefan Bradl <stefan_bradl@web.de>
2012-03-22 15:32:24 +0100

    -

--- src/main/scala/eu/sbradl/liftedcontent/util/RssFeed.scala
+++ src/main/scala/eu/sbradl/liftedcontent/util/RssFeed.scala
@@ -1,5 +1,31 @@
 package eu.sbradl.liftedcontent.util
 
-class RssFeed {
-
+object RssFeed {
+  def apply[T](title: String, link: String, desc: String, language: String, author: String, items: Seq[T],
+      titleOf: (T) => String, descriptionOf: (T) => String, linkTo: (T) => String,
+      authorOf: (T) => String, pubDateOf: (T) => String) = {
+	  <rss version="2.0">
+	  	<channel>
+	  		<title>{title}</title>
+	  		<link>{link}</link>
+	  		<description>{desc}</description>
+	  		<language>{language}</language>
+	  		<copyright>{author}</copyright>
+	  		
+	  		{
+	  		  items map {
+	  		    item => {
+	  		      <item>
+	  		    	<title>{titleOf(item)}</title>
+	  		    	<description>{descriptionOf(item)}</description>
+	  		    	<link>{linkTo(item)}</link>
+	  		    	<author>{authorOf(item)}</author>
+	  		    	<pubDate>{pubDateOf(item)}</pubDate>
+	  		    </item>
+	  		    }
+	  		  }
+	  		}
+	  	</channel>
+	  </rss>
+  }
 }
