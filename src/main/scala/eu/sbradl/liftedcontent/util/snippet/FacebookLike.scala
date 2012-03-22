3a3b54f78c7ac023dd47e5eaf4120f9bef40ed23
Stefan Bradl <stefan_bradl@web.de>
2012-03-22 14:24:51 +0100

    -

--- src/main/scala/eu/sbradl/liftedcontent/util/snippet/FacebookLike.scala
+++ src/main/scala/eu/sbradl/liftedcontent/util/snippet/FacebookLike.scala
@@ -1,9 +1,17 @@
 package eu.sbradl.liftedcontent.util.snippet
 
+import net.liftweb.http.S
+
 class FacebookLike {
 
   def render = {
-    <div id="fb-root"></div>
+    val send = S.attr("send").openOr("true")
+    val layout = S.attr("layout").openOr("standard")
+    val showFaces = S.attr("show-faces").openOr("true")
+    val verb = S.attr("verb").openOr("like")
+    val color = S.attr("color").openOr("light")
+
+    <tail><div id="fb-root"></div>
     <script>
       (function(d, s, id) {{
 	  var js, fjs = d.getElementsByTagName(s)[0];
@@ -12,7 +20,9 @@ class FacebookLike {
 	  js.src = "//connect.facebook.net/de_DE/all.js#xfbml=1";
 	  fjs.parentNode.insertBefore(js, fjs);
 	}}(document, 'script', 'facebook-jssdk'));
-    </script>
-    <div class="fb-like" data-send="true" data-width="450" data-show-faces="true"></div>
+    </script></tail>
+    <div class="fb-like" 
+    	data-send={ send } data-layout={ layout } data-show-faces={ showFaces }
+    	data-action={verb} data-colorscheme={color}></div>
   }
 }
